015de3ab4189bbd879733000ea3054db435f9d22
anjandash <anjan8@gmail.com>
2017-11-11 02:13:29 +0100

    more styles

---
+++ frontend/lib/js/main.js
@@ -0,0 +1,8 @@
+const r = new Router(
+  {
+    about: new Layout(new Page('menu.html'), new Page('about.html')),
+    home: new Layout(new Page('menu.html'), new Page('home.html')),
+    '#default': new Page('menu.html'),
+  },
+  document.querySelector('main')
+);
