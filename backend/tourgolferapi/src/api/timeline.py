11f8f97836d7e3933ae33540f5e4f23fe287b3ee
Igor Jeremic <igor@digitalcube.rs>
2017-11-11 14:18:10 +0100

    i

--- backend/tourgolferapi/src/api/timeline.py
+++ backend/tourgolferapi/src/api/timeline.py
@@ -6,6 +6,7 @@ from base.application.components import params
 from base.application.components import authenticated
 
 from sqlalchemy import desc
+import hashlib
 
 import src.common as common
 
@@ -26,6 +27,7 @@ class Status(Base):
 
         oTimeline, _session = base.common.orm.get_orm_model('timeline')
         oFollowers, _session = base.common.orm.get_orm_model('followers')
+        oUser, _session = base.common.orm.get_orm_model('users')
 
         followers = set()
 
@@ -36,8 +38,22 @@ class Status(Base):
         for t in _session.query(oTimeline).order_by(desc(oTimeline.id)).all():
             j = json.loads(t.json_data)
             if t.id_user in followers or not t.id_user:
+
+                user = _session.query(oUser).filter(oUser.id == t.id_user).one_or_none()
+
+                if user:
+                    user = {
+                        'id': user.id,
+                        'first_name': user.first_name.replace('%20', ' ') if user.first_name else '',
+                        'last_name': user.last_name.replace('%20', ' ') if user.last_name else '',
+                        'image': hashlib.md5(user.auth_user.username.encode()).hexdigest() if user.have_picture else 'avatar'
+                    }
+
                 res.append({'action': t.action,
+                            'user': user,
+                            'id_user': t.id_user,
                             'time': str(t.timestamp),
-                            'text': j['text']})
+                            'text': j['text'].replace('%20',' ')
+                            })
 
         return self.ok({'timeline': res})
